d33fe10239254710c384281ea177e34a97d54e84
WojtekKamilowski <>
2023-07-25 14:37:53 +0000

    Add path to env.py in settings.py

--- hi_story_book_store/settings.py
+++ hi_story_book_store/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+if os.path.isfile('env.py'):
+    import env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ae)9(ei(3z9x#zr3hnqs5ywq%jjer0-dd_gan+hnmsfz*5znh'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
