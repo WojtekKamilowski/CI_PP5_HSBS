b997556dadefeef3ee40405d64aab526b80fd1d8
WojtekKamilowski <>
2023-07-26 22:06:26 +0000

    Customize admin.py

--- books/admin.py
+++ books/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 from .models import Book, Era
 
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Era)
+
+# Inspired by Code Institue's "Boutique Ado" Walkthrough Project
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'era',
+        'price',
+        'rating',
+        'image',
+        'author',
+        'published',
+    )
+
+    ordering = ('sku',)
+
+
+class EraAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Era, EraAdmin)
