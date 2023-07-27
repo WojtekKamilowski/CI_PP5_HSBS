5a835706c76a8be690db2ef0033faa9cbe315572
WojtekKamilowski <>
2023-07-27 15:37:53 +0000

    Add search functionality

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 # Create your views here.
@@ -9,12 +11,27 @@ from .models import Book
 def all_books(request):
     """
     Displays all books
+    Includes sorting & search queries
     """
 
     books = Book.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('books'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            books = books.filter(queries)
 
     context = {
         'books': books,
+        'search_term': query,
     }
 
     return render(request, 'books/books.html', context)
