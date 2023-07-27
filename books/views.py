97fd00ac9e1e18534b136816637a2eda9de9770b
WojtekKamilowski <>
2023-07-27 14:31:59 +0000

    Add product details functionality

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 # Create your views here.
+
+# Based on Code Institue's "Boutique Ado" Walkthrough Project
 
 
 def all_books(request):
@@ -16,3 +18,17 @@ def all_books(request):
     }
 
     return render(request, 'books/books.html', context)
+
+
+def book_detail(request, book_id):
+    """
+    Books view to display details of a single book
+    """
+
+    book = get_object_or_404(Book, pk=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
