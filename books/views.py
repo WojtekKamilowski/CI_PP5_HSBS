e86fccff37afd17c845368566e58c86678287abf
WojtekKamilowski <>
2023-07-27 16:23:40 +0000

    Add sorting functionality

--- books/views.py
+++ books/views.py
@@ -17,8 +17,23 @@ def all_books(request):
     books = Book.objects.all()
     query = None
     eras = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                books = books.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
+
         if 'era' in request.GET:
             eras = request.GET['era'].split(',')
             books = books.filter(era__name__in=eras)
@@ -35,10 +50,13 @@ def all_books(request):
                 description__icontains=query)
             books = books.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'books': books,
         'search_term': query,
         'current_eras': eras,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'books/books.html', context)
