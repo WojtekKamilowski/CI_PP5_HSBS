39ebd2db6d88153337d29582bf7e214119212291
WojtekKamilowski <>
2023-07-29 12:00:27 +0000

    Add add to shopping cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,18 +11,18 @@ def cart_contents(request):
     cart_items = []
     total = 0
     book_count = 0
-    # cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
 
-    # for item_id, item_data in cart.items():
-    #     if isinstance(item_data, int):
-    #         book = get_object_or_404(Book, pk=item_id)
-    #         total += item_data * book.price
-    #         book_count += item_data
-    #         cart_items.append({
-    #             'item_id': item_id,
-    #             'quantity': item_data,
-    #             'book': book,
-    #         })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            book = get_object_or_404(Book, pk=item_id)
+            total += item_data * book.price
+            book_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'book': book,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- cart/views.py
+++ cart/views.py
@@ -4,10 +4,35 @@ from django.shortcuts import (
 from django.contrib import messages
 from books.models import Book
 
+# Based on Code Institue's "Boutique Ado" Walkthrough Project
+
 
 def view_cart(request):
-    """ 
+    """
     Renders the cart
     """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """
+    Adds books to the to shopping cart
+    """
+
+    book = get_object_or_404(Book, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+        messages.success(request,
+                         (f'Updated {book.name} '
+                          f'quantity to {cart[item_id]}'))
+    else:
+        cart[item_id] = quantity
+        messages.success(request, f'Added {book.name} to your shopping cart')
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
