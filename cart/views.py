547102f224194359a0976bd00cb21bfda94dcb23
WojtekKamilowski <>
2023-07-30 11:32:27 +0000

    Add remove items from shopping cart functionality

--- cart/views.py
+++ cart/views.py
@@ -60,3 +60,23 @@ def update_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """
+    Removes the item from the shopping cart
+    """
+
+    try:
+        book = get_object_or_404(Book, pk=item_id)
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+        messages.success(request, f'Removed {book.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
