5422f10cd59489170127fa58624dbc599084c8bc
WojtekKamilowski <>
2023-08-03 08:12:00 +0000

    Add & test basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,12 @@
+# 3rd Party imports
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
-
+import stripe
+import json
+# Internal imports
 from .forms import OrderForm
+from cart.contexts import cart_contents
 
 
 # Based on Code Institue's "Boutique Ado" Walkthrough Project
@@ -16,11 +20,25 @@ def checkout(request):
         messages.error(request, "The shopping cart is empty at the moment")
         return redirect(reverse('books'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
