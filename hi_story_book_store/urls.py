8d9add1d74ee62859d0341cbcbda64003d542a66
WojtekKamilowski <>
2023-08-20 11:44:46 +0000

    Test 404 page & re-ty without views/url

--- hi_story_book_store/urls.py
+++ hi_story_book_store/urls.py
@@ -1,10 +1,7 @@
-# 3rd Party imports
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# Internal imports
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +12,3 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'hi_story_book_store.views.handler404'
